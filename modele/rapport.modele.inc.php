9b3ca3c3f45edeec25bf57e5fa028e95bd66ca81
Baptiste BRUGOUX <brugoux@infogestion.local>
2022-11-23 14:28:43 +0100

    test

--- modele/rapport.modele.inc.php
+++ modele/rapport.modele.inc.php
@@ -1,21 +1,28 @@
 <?php
 include_once 'bd.inc.php';
-
+/**
+ * Undocumented function
+ *
+ * @param [type] $date1
+ * @param [type] $date2
+ * @param [type] $matricule
+ * @return void
+ */
 function getRapportVisite($date1,$date2,$matricule){
 
     try{
         $monPdo=connexionPDO();
-        $req='SELECT RAP_NUM, praticien.PRA_NUM, PRA_NOM, MOT_LIBELLE, RAP_DATEVISITE, MED_NOMCOMMERCIAL
+        $req=$monPdo->prepare('SELECT RAP_NUM, praticien.PRA_NUM, PRA_NOM, MOT_LIBELLE, RAP_DATEVISITE
         FROM rapport_visite
         INNER JOIN praticien
         ON rapport_visite.PRA_NUM=praticien.PRA_NUM
         INNER JOIN motifs
         ON rapport_visite.MOT_ID=motifs.MOT_ID
-        INNER JOIN medicament
-        ON rapport_visite.MED_DEPOTLEGAL_1=medicament.MED_DEPOTLEGAL
-        INNER JOIN medicament
-        ON rapport_visite.MED_DEPOTLEGAL_2=medicament.MED_DEPOTLEGAL';
-
+        WHERE COL_MATRICULE=:matricule AND RAP_DATEVISITE BETWEEN :date1 AND :date2');
+        $req->bindValue(':matricule',$matricule,PDO::PARAM_STR);
+        $req->bindValue(':date1',$date1,PDO::PARAM_STR);
+        $req->bindValue(':date2',$date2,PDO::PARAM_STR);
+        $res=$req->fetchAll(PDO::FETCH_ASSOC);
     }
     catch (PDOException $e){
         print "Erreur !: " . $e->getMessage();
