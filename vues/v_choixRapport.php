958b74378f563a6aa8d7fd2219c98631943af7f8
Léo Lanchard <lanchard@infogestion.local>
2023-04-05 16:51:10 +0200

    version 1.0

--- vues/v_choixRapport.php
+++
@@ -1,8 +0,0 @@
-<div class="d-grid gap-2 col-6 mx-auto">
-    <a href="index.php?uc=rapports&action=saisirRapp" type="button" class="btn btn-primary btn-lg">
-        Saisir un rapport de visite
-    </a>
-    <a href="index.php?uc=rapports&action=formulaireRapport" type="button" class="btn btn-primary btn-lg">
-        Consulter les rapports de visite
-    </a>
-</div>
